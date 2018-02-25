08ac2e7624f4b8055a0bf686ea9ae4e5b0bd1c44
werty <sukumarapptest@gmail.com>
2018-02-25 19:44:34 +0530

    data table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { DataComponent } from './components/data/data.component'
 import { ListenerService } from './services/listener/listener.service'
 import { MaterialModule } from './modules/material.module'
 import { NgModule } from '@angular/core'
+import { reducers } from '../store/reducers/reducers'
+import { StoreModule } from '@ngrx/store'
 import { WebSocketService } from './services/web-socket/web-socket.service'
 
 
@@ -14,7 +16,8 @@ import { WebSocketService } from './services/web-socket/web-socket.service'
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    StoreModule.forRoot(reducers)
   ],
   providers: [WebSocketService, ListenerService],
   bootstrap: [AppComponent]
--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,13 @@
+import * as _ from 'lodash'
+import AddDataAction from '../../../store/actions/data/adddata.action'
+import AppState from '../../../shared/interfaces/app.state'
+import Data from '../../../shared/pojos/data'
+import DataState from '../../../shared/interfaces/data.state'
 import { Component, OnInit } from '@angular/core'
 import { ListenerService } from '../../services/listener/listener.service'
+import { MatTableDataSource } from '@angular/material'
+import { Observable } from 'rxjs/Observable'
+import { Store } from '@ngrx/store'
 import { Subject } from 'rxjs/Subject'
 
 @Component({
@@ -9,15 +17,38 @@ import { Subject } from 'rxjs/Subject'
 })
 
 export class DataComponent implements OnInit {
-  private data
+  public data: Data[]
+  public dataSource = new MatTableDataSource()
+  public sortStr
 
   constructor(
-    private listenerService: ListenerService
-  ) { }
+    private listenerService: ListenerService,
+    private store: Store<AppState>
+  ) {
+    this.store.select('dataState').subscribe((dataState: DataState) => {
+      this.sortStr = dataState.sortType.sortString
+
+      switch (dataState.sortType.order) {
+        case 'time_up':
+          this.data = _.sortBy(dataState.data, 'timestamp').reverse()
+          break
+        case 'time_down':
+          this.data = _.sortBy(dataState.data, 'timestamp')
+          break
+        case 'close_up':
+          this.data = _.sortBy(dataState.data, 'close').reverse()
+          break
+        case 'close_down':
+          this.data = _.sortBy(dataState.data, 'close')
+          break
+      }
+    })
+  }
+
 
   ngOnInit() {
-    this.listenerService.messages.subscribe(msg => {
-      console.log(msg)
+    this.listenerService.data.subscribe(newData => {
+      this.store.dispatch(new AddDataAction(newData))
     })
   }
 }
